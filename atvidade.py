989c95fdc6806019b88f010642c61c8f4c67957f
ArtAulas <carelliarthur@gmail.com>
2023-10-30 10:29:06 -0300

    Commit 2/3

--- atvidade.py
+++ atvidade.py
@@ -7,6 +7,13 @@ def multiplos(a):
         aux+=1
     return multiplos
 
+def divisores(a):
+    divisores=[]
+    for n in range(1,11):
+            if a%n==0:
+                divisores.append(n)
+    return divisores
+
 def executa():
     n1=input("Entre o número:\n")
     try:
@@ -14,6 +21,7 @@ def executa():
     except:
         print("Insira um número")
         return
-    print(multiplos(int(n1)))
+    print("Multiplos:",multiplos(int(n1)))
+    print("Divisores",divisores(int(n1)))
 
 executa()
